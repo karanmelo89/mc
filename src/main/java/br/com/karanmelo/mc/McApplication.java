d1ce3729f2e8cea76fdd8e7a7f98babf9062b9f0
Karan Melo <karan@karanmelo.com.br>
2019-01-16 23:22:51 -0300

    Projeto criado

---
+++ src/main/java/br/com/karanmelo/mc/McApplication.java
@@ -0,0 +1,14 @@
+package br.com.karanmelo.mc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/br/com/karanmelo/mc/McApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.karanmelo.mc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class McApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
